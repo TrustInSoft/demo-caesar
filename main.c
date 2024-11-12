0c3ff0ff7ed38585b15342db0835da9bc2400e21
Dorian Haglund <dorian.haglund@trust-in-soft.com>
2024-11-12 14:27:29 +0100

    Remove former tests.

--- main.c
+++ main.c
@@ -6,13 +6,8 @@ void gen_test(char *str, size_t str_len, int shift)
 {
     char *res1, *res2;
 
-    printf("Encrypt text '%s'\n", str);
     res1 = caesar_encrypt(str, str_len, shift);
-    printf("Result:       %s\n", res1);
-
-    printf("Decrypt text '%s'\n", res1);
     res2 = caesar_decrypt(res1, str_len, shift);
-    printf("Result:       %s\n", res2);
 
     free(res1);
     free(res2);
@@ -25,8 +20,6 @@ int main(void)
 
     printf("Test 1: Shift with a negative input\n");
     gen_test(orig_str, str_len, -3);
-    printf("\nTest 2: Shift with a positive input\n");
-    // gen_test(orig_str, str_len, 7);
 
     return 0;
 }
